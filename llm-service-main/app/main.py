5ed0427ee7fe1bcc5a03d66a7c4f71587e7eafec
SamuraiPetya <mazur.dmitry03@gmail.com>
2025-06-09 01:28:38 +0700

    need to add bd for tests via data service

--- llm-service-main/app/main.py
+++ llm-service-main/app/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from .api.v1.endpoints import router as v1_router
 from contextlib import asynccontextmanager
 from .services.llm_service import init_prompts
+from fastapi.middleware.cors import CORSMiddleware
+
 
 
 @asynccontextmanager
@@ -12,5 +14,18 @@ async def lifespan(app: FastAPI):
     print("Shutdown complete")
 
 app = FastAPI(title="LLM Service", lifespan=lifespan)
+
+origins = [
+    "http://localhost",# Адрес вашего фронтенда
+    "null"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(v1_router, prefix="/api/v1")
